ebbb8a09b4b8717d86a17b3890ab343ae6a4fd6b
Akshaya <akshaya@Akshayas-MacBook-Air.local>
2026-08-19 17:03:18 +0530

    feat: Created App database

--- app/src/main/java/com/example/trail/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/trail/data/local/AppDatabase.kt
@@ -1,2 +1,34 @@
 package com.example.trail.data.local
 
+import android.content.Context //gives code access to android
+import androidx.room.Database //this class represents my database
+import androidx.room.Room //this imports the room class
+import androidx.room.RoomDatabase //this is the parent class inherted by AppDatabse
+
+@Database(
+    entities = [BookEntity::class, UserBookStateEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun bookDao(): BookDao
+    abstract fun userBookStateDao(): UserBookStateDao
+
+    companion object { // shared area inside the class, call a function without creating a object
+        @Volatile
+        private var INSTANCE: AppDatabase? = null //Intsnce store the databse obj
+
+        fun getInstance(context: Context): AppDatabase { //give me the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "books_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
