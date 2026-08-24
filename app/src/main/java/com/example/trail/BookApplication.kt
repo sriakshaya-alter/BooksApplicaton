0b41e574057183c0fd0a05efd3f1fe5633951426
Akshaya <akshaya@Akshayas-MacBook-Air.local>
2026-08-24 20:53:28 +0530

    feat: changed BookApplication file to work for MVP

--- app/src/main/java/com/example/trail/BookApplication.kt
+++ app/src/main/java/com/example/trail/BookApplication.kt
@@ -1,7 +1,8 @@
 package com.example.trail
 
 import android.app.Application
-import com.example.trail.shared.AppDatabase
+import com.example.trail.shared.appContext
+import com.example.trail.shared.getDatabaseBuilder
 import com.example.trail.data.Repository.BookRepository
 import com.example.trail.data.DataStore.SettingsDataStore
 import com.example.trail.shared.BookApiService
@@ -9,12 +10,17 @@ import com.example.trail.shared.createHttpClient
 
 class BookApplication : Application() {
 
-    val database by lazy { AppDatabase.getInstance(this) }
+    val database by lazy { getDatabaseBuilder().build() }
     val repository by lazy { BookRepository(database) }
 
     val settingsDataStore by lazy { SettingsDataStore(this) }
 
     val bookApiService by lazy {
         BookApiService(createHttpClient())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        appContext = this  // set Android context for Room to use
     }
 }
