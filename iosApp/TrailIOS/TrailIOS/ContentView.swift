8ee265fb64bdac44e0fc5dfe101bd47f4a6fe8dd
Akshaya <akshaya@Akshayas-MacBook-Air.local>
2026-08-25 11:27:36 +0530

    feat: add iOS Xcode project with sharedKit framework integration

---
+++ iosApp/TrailIOS/TrailIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrailIOS
+//
+//  Created by Akshaya on 25/08/26.
+//
+
+import sharedKit
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           Text("Tail Books")
+                .font(.largeTitle)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
@@ -38,24 +40,29 @@ kotlin {
     // project can be found here:
     // https://developer.android.com/kotlin/multiplatform/migrate
     val xcfName = "sharedKit"
+    val xcf = XCFramework(xcfName)
 
     iosX64 {
         binaries.framework {
             baseName = xcfName
+            xcf.add(this)
         }
     }
 
     iosArm64 {
         binaries.framework {
             baseName = xcfName
+            xcf.add(this)
         }
     }
 
     iosSimulatorArm64 {
         binaries.framework {
             baseName = xcfName
+            xcf.add(this)
         }
     }
+
 
     // Source set declarations.
     // Declaring a target automatically creates a source set with the same name. By default, the
